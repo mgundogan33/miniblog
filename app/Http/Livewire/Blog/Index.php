b989712c14cf3bcda9b251ee2eb5fbacbe1ce241
mgundogan33 <gundogan.mehmet33@gmail.com>
2022-05-05 11:25:39 +0300

    10

--- app/Http/Livewire/Blog/Index.php
+++ app/Http/Livewire/Blog/Index.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Livewire\Blog;
 
+use App\Models\Blog;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
     public function render()
     {
-        return view('livewire.blog.index');
+        $data = array('blogs' => Blog::latest()->paginate(6));
+        return view('livewire.blog.index', $data);
     }
 }
