503fa5869ee62832b7ed83f2899720edd1ed16c7
mgundogan33 <gundogan.mehmet33@gmail.com>
2022-05-05 17:04:24 +0300

    16

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,9 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    <div class="container">
+    <div class="button">
+        <a href="{{route('blog')}}" class="btn btn-success">Anasayfa</a>
+        </div>
+    </div>
 
     <div class="container mt-4">
         <div class="row">
